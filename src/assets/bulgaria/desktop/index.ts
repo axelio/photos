6a06c1002836169595f942fa979c6c40003b0cf8
axelio <axel.szweda@gmail.com>
2019-09-22 22:58:55 +0200

    updated sofia gallery

--- src/assets/bulgaria/desktop/index.ts
+++ src/assets/bulgaria/desktop/index.ts
@@ -2,6 +2,7 @@ import img1 from './1.jpeg';
 import img2 from './2.jpeg';
 import img3 from './3.jpeg';
 import img4 from './4.jpeg';
+import img5 from './5.jpeg';
 import img6 from './6.jpeg';
 import img8 from './8.jpeg';
 import img9 from './9.jpeg';
@@ -11,5 +12,5 @@ import img12 from './12.jpeg';
 import img13 from './13.jpeg';
 
 export {
-    img1, img2, img3, img4, img6, img8, img9, img10, img11, img12, img13
+    img1, img2, img3, img4, img5, img6, img8, img9, img10, img11, img12, img13
 }
--- src/assets/bulgaria/images.ts
+++ src/assets/bulgaria/images.ts
@@ -9,6 +9,7 @@ const images: PictureWithThumbnail[] = [
     { thumbnail: thumbnails.img2, photo: { desktop: desktopPhotos.img2, mobile: mobilePhotos.img2 } },
     { thumbnail: thumbnails.img3, photo: { desktop: desktopPhotos.img3, mobile: mobilePhotos.img3 } },
     { thumbnail: thumbnails.img4, photo: { desktop: desktopPhotos.img4, mobile: mobilePhotos.img4 } },
+    { thumbnail: thumbnails.img5, photo: { desktop: desktopPhotos.img5, mobile: mobilePhotos.img5 } },
     { thumbnail: thumbnails.img6, photo: { desktop: desktopPhotos.img6, mobile: mobilePhotos.img6 } },
     { thumbnail: thumbnails.img8, photo: { desktop: desktopPhotos.img8, mobile: mobilePhotos.img8 } },
     { thumbnail: thumbnails.img9, photo: { desktop: desktopPhotos.img9, mobile: mobilePhotos.img9 } },
