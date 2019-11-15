d8ecd83573eb3123f5b542ddeafd9c5a2739df6a
axelitoo <axel.szweda@gmail.com>
2019-11-15 14:21:46 +0100

    added spinner

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Gallery.css';
 import { GalleryProps } from './types';
 import { AppState, Action, ActionNames } from '../../state/types';
 import { StateContext } from '../../state/store';
+import Spinner from '../Spinner/Spinner';
 
 const Gallery: React.FC<GalleryProps> = ({ images }) => {
     const { state, dispatch }: { state: AppState, dispatch: (arg: Action) => void } = useContext(StateContext);
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedImages, setLoadedImages] = useState(0);
 
     useEffect(() => {
         window.scrollTo(0, state.scrollYPosition);
@@ -16,15 +19,21 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
         }
     }, []);
 
+    const imageLoaded = () => {
+        setLoadedImages(loadedImages + 1);
+        if (loadedImages === images.length - 1) setIsLoading(false);
+    }
+
     return (
         <div>
-            <div className='photos'>
+            {isLoading && <div className='spinnerContainer'><Spinner /></div>}
+            <div className='photos' style={{ display: `${isLoading ? 'none' : ''}` }}>
                 {images.map((image, index) =>
                     <Link key={index} className='thumbnail' to={{
                         pathname: '/preview',
                         state: { selectedPhotoIndex: index, images: images }
                     }}>
-                        <img src={image.thumbnail} alt="" />
+                        <img src={image.thumbnail} onLoad={imageLoaded} alt="" />
                     </Link>
                 )}
             </div>
